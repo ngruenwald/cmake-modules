410de40e5e188697ec26c11c7441479e3286100d
ngruenwald <40895194+ngruenwald@users.noreply.github.com>
2023-08-24 22:32:27 +0200

    make ext_backward upgrade script compatible

--- external/ext_backward.cmake
+++ external/ext_backward.cmake
@@ -4,13 +4,13 @@
 include(${CMAKE_CURRENT_LIST_DIR}/external.cmake)
 
 set(EXT_VERSION "1.6")
-set(EXT_HASH "SHA256=9b07e12656ab9af8779a84e06865233b9e30fadbb063bf94dd81d318081db8c2")
+set(EXT_URL_HASH "SHA256=9b07e12656ab9af8779a84e06865233b9e30fadbb063bf94dd81d318081db8c2")
 
 AddExternalProject(
   backward
   UPDATE_DISCONNECTED true
   URL https://github.com/bombela/backward-cpp/archive/refs/tags/v${EXT_VERSION}.zip
-  URL_HASH ${EXT_HASH}
+  URL_HASH ${EXT_URL_HASH}
   CMAKE_ARGS
     -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     -D CMAKE_INSTALL_PREFIX=${EXTERNAL_INSTALL_LOCATION}
