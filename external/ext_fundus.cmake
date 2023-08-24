bfa4381dddc7c4d1041cbfb8e7ff10cb8c7f8a5c
ngruenwald <40895194+ngruenwald@users.noreply.github.com>
2023-08-24 22:34:08 +0200

    make fundus upgrade script compatible

--- external/ext_fundus.cmake
+++ external/ext_fundus.cmake
@@ -3,10 +3,13 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/external.cmake)
 
+set(EXT_VERSION "main")
+set(ET_URL_HASH "")
+
 AddExternalProject(
   fundus
   UPDATE_DISCONNECTED true
-  URL https://github.com/ngruenwald/fundus/archive/refs/heads/main.zip
+  URL https://github.com/ngruenwald/fundus/archive/refs/heads/${EXT_VERSION}.zip
   CMAKE_ARGS
     -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     -D CMAKE_INSTALL_PREFIX=${EXTERNAL_INSTALL_LOCATION}
